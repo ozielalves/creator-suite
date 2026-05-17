00b42453beca84bba2ddf752404e96eea878aded
Oziel Alves <oziel@iconosquare.com>
2026-05-18 00:58:48 +0200

    chore: exporting all types

--- src/modules/Messaging/index.ts
+++ src/modules/Messaging/index.ts
@@ -1,4 +1,4 @@
 export { MessagingPage } from "./pages/MessagingPage";
 export { MediaService, MediaValidationError } from "./services/MediaService";
 export * from "./constants";
-export type { Message, MessageAttachment, Conversation } from "./types";
+export type * from "./types";
